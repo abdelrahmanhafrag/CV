eacab5735d55aa89d9c79830024245dbe627ef2a
bidoh <98671305+abdelrahmanhafrag@users.noreply.github.com>
2023-06-19 14:23:16 +0300

    CV project

---
+++ ATSWebAppV2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ATSWebAppV2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
